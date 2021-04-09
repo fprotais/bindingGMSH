4d8ff8a4e9eafcbfb54a8e852b5b6a269ae136c8
fprotais VM <francois.protais@inria.fr>
2021-04-09 18:23:20 +0200

    a bit of gui disp

--- bin/main.cpp
+++ bin/main.cpp
@@ -4,85 +4,141 @@
 #include <string>
 #include <iostream>
 
+#if 0
 namespace fs = std::filesystem;
 
 const std::string Mambo_dir = "../mambo/";
 const std::vector<std::string> model_dir = {"Basic/", "Simple/", "Medium/"};
+void iterate_on_mambo() {
+    for (const std::string& dir : model_dir){
+        for (const auto& entry : fs::directory_iterator(Mambo_dir + dir)) {
+            std::cerr << "Processing: " << entry << std::endl;
+            std::string ext = entry.path().extension().string();
+            std::string filename = entry.path().stem().string();
+            std::string path2file = entry.path().string();
+        }
+    }
+}
+#endif
 
-static void gmsh_simplexmesh_stepmodel(const std::string& inname, const std::string& outname, int dim = 3){
-    gmsh::initialize();
+// in degree
+static void set_rotation(int rotX, int rotY, int rotZ){
+    gmsh::option::setNumber("General.RotationX", rotX);
+    gmsh::option::setNumber("General.RotationY", rotY);
+    gmsh::option::setNumber("General.RotationZ", rotZ);
+}
+static void set_scale(double scale){
+    gmsh::option::setNumber("General.ScaleX", scale);
+    gmsh::option::setNumber("General.ScaleY", scale);
+    gmsh::option::setNumber("General.ScaleZ", scale);
+}
+typedef std::array<double, 3> vec3;
 
-    gmsh::merge(inname);
-    gmsh::option::setNumber("Mesh.MeshSizeFactor", 0.3);
-    gmsh::model::mesh::generate(dim);
-    gmsh::write(outname);
-    gmsh::finalize();
+static void set_plan_clipping(vec3 d, double l){
+    gmsh::option::setNumber("Mesh.Clip", 1);
+    gmsh::option::setNumber("Geometry.Clip", 1);
+    gmsh::option::setNumber("General.ClipWholeElements", 1);
+    gmsh::option::setNumber("General.ClipOnlyVolume", 0);
+
+
+    gmsh::option::setNumber("General.Clip0A", d[0]);
+    gmsh::option::setNumber("General.Clip0B", d[1]);
+    gmsh::option::setNumber("General.Clip0C", d[2]);
+    gmsh::option::setNumber("General.Clip0D", l);
+
+
+}
+static void set_box_clipping(vec3 A, vec3 B){
+    gmsh::option::setNumber("Mesh.Clip", 63);
+    gmsh::option::setNumber("Geometry.Clip", 63);
+    gmsh::option::setNumber("General.ClipWholeElements", 1);
+    gmsh::option::setNumber("General.ClipOnlyVolume", 0);
+
+    gmsh::option::setNumber("General.Clip0D", A[0]);
+    gmsh::option::setNumber("General.Clip1D", A[1]);
+    gmsh::option::setNumber("General.Clip2D", A[2]);
+    gmsh::option::setNumber("General.Clip3D", B[0]);
+    gmsh::option::setNumber("General.Clip4D", B[1]);
+    gmsh::option::setNumber("General.Clip5D", B[2]);
+}
+static void turnoff_clipping(){
+    gmsh::option::setNumber("Mesh.Clip", 0);
+    gmsh::option::setNumber("Geometry.Clip", 0);
 }
 
-static void gmsh_quadmesh_stepmodel(const std::string& inname, const std::string& outname){
-
-    gmsh::initialize();
-
-    gmsh::merge(inname);
-    gmsh::option::setNumber("Mesh.MeshSizeFactor", 0.1);
-    gmsh::option::setNumber("General.AbortOnError", 1);
-    gmsh::option::setNumber("General.Terminal", 1);
-    gmsh::option::setNumber("General.ExpertMode", 1);
-    gmsh::option::setNumber("Mesh.Algorithm",11);
-    // gmsh::option::setNumber("Mesh.MeshSizeMin", 3);
-    // gmsh::option::setNumber("Mesh.MeshSizeMax", 3);
-
-    gmsh::model::mesh::generate(2);
-    gmsh::write(outname);
-    
-    gmsh::finalize();
-    return;
-}
 
 std::vector<std::pair<std::string, int>> scene_settings =  {
     {"General.BackgroundGradient", 0},
     {"General.Antialiasing", 1},
     {"General.Trackball",0},
-    {"General.SmallAxes",0},
-    {"Geometry.Points", 0},
+    // {"General.SmallAxes",0},
+
+    {"Geometry.Points", 1},
+    {"Geometry.PointSize", 10},
+    {"Geometry.PointType", 1},
+
     {"Geometry.Lines", 1},
-    {"Geometry.LineWidth", 1.2},
+    {"Geometry.LineWidth", 2},
     {"Geometry.LineType", 1},
+
     {"Geometry.NumSubEdges", 200},
     {"Mesh.ColorCarousel", 0},
+
     {"Mesh.LineWidth", 1.2},
-    {"Mesh.SurfaceFaces",1},
+
     {"Geometry.Lines", 1},
     {"Geometry.Surfaces", 0},
     {"Geometry.SurfaceType", 2},
+
+    {"Mesh.SurfaceFaces",0},
+    {"Mesh.SurfaceEdges",0},
+
+    {"Mesh.VolumeFaces",1},
+    {"Mesh.VolumeEdges",1},
+
 };
 std::vector<std::pair<std::string, std::array<int, 3>>> scene_colors =  {
     {"Mesh.Quadrangles", {242,242,242}},
+    {"Mesh.Tetrahedra", {242,242,242}},
     {"Geometry.Lines", {0, 160, 220}},
+    {"Geometry.Points", {90, 90, 90}},
 };
-static void test_gui_playing(const std::string& inname, const std::string& imagename){
-    gmsh::initialize();
+static void test_gui_playing(const std::string& stepmodel, const std::string& meshmodel){
+    gmsh::initialize(0,nullptr, false);
+    gmsh::fltk::update();
+    gmsh::merge(stepmodel);
+    gmsh::fltk::update();
+    gmsh::merge(meshmodel);
+    gmsh::fltk::update();
+    for (auto p : scene_settings) gmsh::option::setNumber(p.first, p.second);
+    for (auto p : scene_colors) gmsh::option::setColor(p.first, p.second[0],p.second[1],p.second[2]);
+    set_rotation(0, 60, 30);
+    set_scale(0.8);
+    gmsh::fltk::update();
 
-    gmsh::merge(inname);
+    int rot = 60;
+    while (gmsh::fltk::isAvailable()) {
+        std::cerr << cos(rot*2*M_PI/360) << " : " << sin(rot*2*M_PI/360) << std::endl;
+        set_plan_clipping({cos(rot*2*M_PI/360),0,sin(rot*2*M_PI/360)}, 1);
+        // set_rotation(0, rot, 30);
+        rot = (rot+1) % 360;
+        gmsh::fltk::update();
+        gmsh::graphics::draw();
+    }
     gmsh::finalize();
 
 }
 
-int main(int argc, char** argv) {
-    int ct = 0;
-    int mesh_nb = 0;
-    if (argc == 2) mesh_nb = std::stoi(argv[1]); 
-    for (const std::string& dir : model_dir){
-        for (const auto& entry : fs::directory_iterator(Mambo_dir + dir)) {
-            if (ct++ != mesh_nb) continue;
-            std::cerr << "Processing: " << entry << std::endl;
-            std::string ext = entry.path().extension().string();
-            std::string filename = entry.path().stem().string();
-            gmsh_quadmesh_stepmodel(entry.path().string(), filename + "_quad.mesh");
-            // gmsh_simplexmesh_stepmodel(entry.path().string(), filename + ".mesh");
-        }
-    }
 
+
+int main(int argc, char** argv) {
+    if (argc != 3){
+        std::cout << "Usage: " << argv[0] << " mesh.step  mesh.mesh (or msh supported format)" << std::endl;
+        return 1;
+    } 
+    std::string stepmodel = argv[1]; 
+    std::string meshmodel = argv[2]; 
+    test_gui_playing(stepmodel, meshmodel);
     return 0;
 }
 
